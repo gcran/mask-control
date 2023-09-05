4500f6c8cefc676e9bf28b42a8c9cb87bf527b99
Graham Cran <graham.cran@gmail.com>
2023-09-04 22:50:59 -0400

    Script tweak

--- scripts/welcomeScript.py
+++ scripts/welcomeScript.py
@@ -8,9 +8,8 @@ def welcomeScript(robot):
     robot.setMotorCmd('eyelids', robot.motors['eyelids'].llim_angle)
     robot.setMotorCmd('head_yaw', robot.motors['head_yaw'].init_angle)
     robot.setMotorCmd('eyes', robot.motors['eyes'].init_angle)
-    robot.setLightCmd((robot.LIGHT_MIN, robot.LIGHT_MIN, robot.LIGHT_MIN))
-    
-    while((abs(robot.motors['eyelids'].getErr()) > 1)):
+    time.sleep(robot.update_period)
+    while((abs(robot.motors['eyelids'].getErr()) > 1) or (abs(robot.motors['head_yaw'].getErr()) > 1) or (abs(robot.motors['eyes'].getErr()) > 1)):
         time.sleep(robot.update_period)
         
     time.sleep(1)
@@ -18,9 +17,8 @@ def welcomeScript(robot):
     #wake up
     print('wake up')
     robot.setMotorCmd('eyelids', robot.motors['eyelids'].ulim_angle)
-    robot.setLightCmd((robot.LIGHT_MIN, robot.LIGHT_MIN, robot.LIGHT_MIN))
     robot.setLightCmd((robot.LIGHT_MIN, robot.LIGHT_MAX, robot.LIGHT_MIN))
-    
+    time.sleep(robot.update_period)
     while((abs(robot.motors['eyelids'].getErr()) > 1)):
         time.sleep(robot.update_period)
     time.sleep(1)
@@ -37,14 +35,22 @@ def welcomeScript(robot):
     while((abs(robot.motors['eyelids'].getErr()) > 1)): 
         time.sleep(robot.update_period)
         
+    
+    #blink
+    print('flip head')
+    robot.setPersonality(robot.EVIL)
+    time.sleep(robot.update_period)
+    while((abs(robot.motors['head_roll'].getErr()) > 1)):
+        time.sleep(robot.update_period)
+        
     # eyes move left and at halfway point, trigger head yaw left
     print('eyes move left and at halfway point, trigger head yaw left')
-    robot.setMotorCmd('eyes', (robot.motors['eyes'].llim_angle + robot.motors['eyes'].init_angle) / 2)
+    robot.setMotorCmd('eyes', (robot.motors['eyes'].ulim_angle + robot.motors['eyes'].init_angle) / 2)
     time.sleep(robot.update_period)
     while((abs(robot.motors['eyes'].getErr()) > 1)):
         time.sleep(robot.update_period)
         
-    robot.setMotorCmd('eyes', robot.motors['eyes'].llim_angle)
+    robot.setMotorCmd('eyes', robot.motors['eyes'].ulim_angle)
     robot.setMotorCmd('head_yaw', robot.motors['head_yaw'].llim_angle)
     time.sleep(robot.update_period)
     while((abs(robot.motors['eyes'].getErr()) > 1) or (abs(robot.motors['head_yaw'].getErr()) > 1)):
@@ -64,12 +70,12 @@ def welcomeScript(robot):
         
     # eyes move right and at halfway point, trigger head yaw right
     print('eyes move right and at halfway point, trigger head yaw right')
-    robot.setMotorCmd('eyes', (robot.motors['eyes'].ulim_angle + robot.motors['eyes'].init_angle) / 2)
+    robot.setMotorCmd('eyes', (robot.motors['eyes'].llim_angle + robot.motors['eyes'].init_angle) / 2)
     time.sleep(robot.update_period)
     while((abs(robot.motors['eyes'].getErr()) > 1)):
         time.sleep(robot.update_period)
         
-    robot.setMotorCmd('eyes', robot.motors['eyes'].ulim_angle)
+    robot.setMotorCmd('eyes', robot.motors['eyes'].llim_angle)
     robot.setMotorCmd('head_yaw', robot.motors['head_yaw'].ulim_angle)
     time.sleep(robot.update_period)
     while((abs(robot.motors['eyes'].getErr()) > 1) or (abs(robot.motors['head_yaw'].getErr()) > 1)):
@@ -104,13 +110,13 @@ def welcomeScript(robot):
     robot.playSound('welcome')
     time.sleep(robot.update_period)
     while (robot.isTalking()):
-        mouth_command = ((robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle)  * math.sin(time.time() % math.tau)) + (robot.motors['mouth'].llim_angle + (robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle) / 2)
+        mouth_command = ((robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle)  * math.sin((time.time()*4) % math.tau)) + (robot.motors['mouth'].llim_angle + (robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle) / 2)
         robot.setMotorCmd('mouth', mouth_command)
         
     robot.playSound('all that is left')
     time.sleep(robot.update_period)
     while (robot.isTalking()):
-        mouth_command = ((robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle)  * math.sin(time.time() % math.tau)) + (robot.motors['mouth'].llim_angle + (robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle) / 2)
+        mouth_command = ((robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle)  * math.sin((time.time()*4) % math.tau)) + (robot.motors['mouth'].llim_angle + (robot.motors['mouth'].ulim_angle - robot.motors['mouth'].llim_angle) / 2)
         robot.setMotorCmd('mouth', mouth_command)
         
     #blink
